5fc3af987f402e663ce61007c3449fa2a42c93de
カイル <kyle@kylerich.dev>
2026-02-05 12:38:33 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,55 @@
 Unregister-ScheduledTask -TaskName "Arenarr" -Confirm:$false -ErrorAction SilentlyContinue
-Register-ScheduledTask -TaskName "Arenarr" -Action (New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -Command `"Invoke-Expression (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/imkylecat/Arenarr/refs/heads/main/install.ps1' -UseBasicParsing).Content`"") -Trigger (New-ScheduledTaskTrigger -AtStartup),(New-ScheduledTaskTrigger -AtLogOn),(New-ScheduledTaskTrigger -AtIdle) -Principal (New-ScheduledTaskPrincipal -UserId "$env:USERNAME" -LogonType Interactive -RunLevel Highest) -Settings (New-ScheduledTaskSettingsSet -Compatibility Win10) -Force
+$taskXml = @"
+<?xml version="1.0" encoding="UTF-16"?>
+<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
+  <RegistrationInfo>
+    <Date>$(Get-Date -Format "yyyy-MM-ddTHH:mm:ss")</Date>
+  </RegistrationInfo>
+  <Triggers>
+    <BootTrigger>
+      <Enabled>true</Enabled>
+    </BootTrigger>
+    <LogonTrigger>
+      <Enabled>true</Enabled>
+      <UserId>$env:USERDOMAIN\$env:USERNAME</UserId>
+    </LogonTrigger>
+    <IdleTrigger>
+      <Enabled>true</Enabled>
+    </IdleTrigger>
+  </Triggers>
+  <Principals>
+    <Principal id="Author">
+      <UserId>$env:USERDOMAIN\$env:USERNAME</UserId>
+      <LogonType>InteractiveToken</LogonType>
+      <RunLevel>HighestAvailable</RunLevel>
+    </Principal>
+  </Principals>
+  <Settings>
+    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
+    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
+    <StopIfGoingOnBatteries>false</StopIfGoingOnBatteries>
+    <AllowHardTerminate>true</AllowHardTerminate>
+    <StartWhenAvailable>true</StartWhenAvailable>
+    <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>
+    <IdleSettings>
+      <StopOnIdleEnd>true</StopOnIdleEnd>
+      <RestartOnIdle>false</RestartOnIdle>
+    </IdleSettings>
+    <AllowStartOnDemand>true</AllowStartOnDemand>
+    <Enabled>true</Enabled>
+    <Hidden>false</Hidden>
+    <RunOnlyIfIdle>false</RunOnlyIfIdle>
+    <WakeToRun>false</WakeToRun>
+    <ExecutionTimeLimit>PT0S</ExecutionTimeLimit>
+    <Priority>7</Priority>
+  </Settings>
+  <Actions Context="Author">
+    <Exec>
+      <Command>powershell.exe</Command>
+      <Arguments>-NoProfile -ExecutionPolicy Bypass -Command "Invoke-Expression (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/imkylecat/Arenarr/refs/heads/main/install.ps1' -UseBasicParsing).Content"</Arguments>
+    </Exec>
+  </Actions>
+</Task>
+"@
+Register-ScheduledTask -TaskName "Arenarr" -Xml $taskXml -Force
 Invoke-Expression (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/imkylecat/Arenarr/refs/heads/main/clean.ps1' -UseBasicParsing).Content
